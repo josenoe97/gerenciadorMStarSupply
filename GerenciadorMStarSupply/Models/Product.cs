17e00fb2888ced53bfd39372f55587e037810683
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-10-07 18:20:07 -0300

    Initial commit

---
+++ GerenciadorMStarSupply/Models/Product.cs
@@ -0,0 +1,13 @@
+﻿namespace GerenciadorMStarSupply.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public int Amount { get; set; }
+
+        public double Price { get; set; }
+    }
+}
