19110a10cece036a1cd81d4c24bc6e32899eb838
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-10-07 22:20:46 -0300

    Create gerenciamento page

---
+++ GerenciadorMStarSupply/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using GerenciadorMStarSupply.Data;
+using GerenciadorMStarSupply.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GerenciadorMStarSupply.Controllers
+{
+    public class ProductController : Controller
+    {
+        readonly private GerenciadorContext _context;
+
+        public ProductController(GerenciadorContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> products = _context.Products;
+            return View(products);
+        }
+    }
+}
