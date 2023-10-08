b0163b2f44ff0dddeb8de7d17bc8a32db345bb3c
Jose Edson Jr <edsonjunior9701@outlook.com>
2023-10-08 00:34:53 -0300

    CRUD and pages

--- GerenciadorMStarSupply/Controllers/ProductController.cs
+++ GerenciadorMStarSupply/Controllers/ProductController.cs
@@ -18,5 +18,79 @@ namespace GerenciadorMStarSupply.Controllers
             IEnumerable<Product> products = _context.Products;
             return View(products);
         }
+
+        public IActionResult Register() 
+        {
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Edit(int? id) 
+        {
+            if(id == null || id == 0)
+                return NotFound();
+            
+
+            Product product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if(product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+
+
+            Product product = _context.Products.FirstOrDefault(p => p.Id == id);
+
+            if (product == null)
+                return NotFound();
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Register(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Add(product);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        public IActionResult Edit(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Products.Update(product);
+                _context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+
+        [HttpPost]
+        public IActionResult Delete(Product product)
+        {
+            if(product == null)
+                return NotFound();
+
+            _context.Products.Remove(product);
+            _context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
     }
 }
--- GerenciadorMStarSupply/Models/Product.cs
+++ GerenciadorMStarSupply/Models/Product.cs
@@ -1,13 +1,19 @@
-﻿namespace GerenciadorMStarSupply.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GerenciadorMStarSupply.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required(ErrorMessage="Digite o nome do Produto!")]
         public string Name { get; set; }
 
+        [Required(ErrorMessage = "Digite a quantidade de Produto!")]
         public int Amount { get; set; }
 
+        [Required(ErrorMessage = "Digite o valor do Preço!")]
         public double Price { get; set; }
     }
 }
